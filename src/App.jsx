d3139557167e6fe3f27b1c4e099f07624f71b364
Preksha <ranapreksha8@gmail.com>
2025-06-24 09:48:00 +0530

    feat: initialize blog application with React, Redux, and Appwrite integration

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect,useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import { logout, login } from './store/authSlice'
+import {Header,Footer} from './components/'
+import './App.css'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .then(() => {
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error)
+        dispatch(logout())
+        setLoading(false)
+      })
+  }, [])
+
+  
+
+
+  return !loading ? (
+    <div className='min-h-screen flex-wrap bg-gray-100 flex items-center justify-center'>
+        <div className='w-full max-w-4xl mx-auto p-4'>
+          <Header />
+          <main className='mt-4'>
+            {/* <Outlet /> */}
+          </main>
+          <Footer />
+          </div>
+    </div>): null
+  
+}
+
+export default App
